3efa2d11d7a21c73d62fff41fe77925408a9217e
Mats Gisselson <mats.gisselson@gmail.com>
2016-10-05 23:26:03 +0200

    Moved helper macros out of main module.

---
+++ src/clojure_perf/macros.clj
@@ -0,0 +1,32 @@
+(ns clojure-perf.macros
+  (:require [clojure.pprint :as pprint]
+            [clojure.string :as string]
+            [criterium.core :as criterium])
+  (:gen-class))
+
+(def ^:private dividing-line-width 80)
+(def dividing-line-thin (apply str (repeat dividing-line-width \-)))
+(def dividing-line-thick (apply str (repeat dividing-line-width \=)))
+
+(defmacro measure
+  [& body]
+  (let [expr (if (second body) (cons 'do body) (first body))]
+    `(do (pprint/write '~expr :dispatch pprint/code-dispatch)
+         (pprint/fresh-line)
+         (let [output# (with-out-str (criterium/quick-bench ~expr))
+               lines# (-> output# string/trim-newline string/split-lines)]
+           (println ";")
+           (doseq [line# lines#]
+             (println ";" line#)))
+         (println ";" dividing-line-thin)
+         (pprint/fresh-line))))
+
+(defmacro defbench
+  [name args & body]
+  `(defn ~name
+     ~args
+     (println)
+     (println ";" '~name)
+     (println ";" dividing-line-thick)
+     (println)
+     ~@body))
--- src/clojure_perf/main.clj
+++ src/clojure_perf/main.clj
@@ -1,36 +1,7 @@
 (ns clojure-perf.main
-  (:require [clojure.pprint :as pprint]
-            [clojure.core.reducers :as r]
-            [clojure.string :as string]
-            [criterium.core :refer [quick-bench with-progress-reporting]])
+  (:require [clojure.core.reducers :as r]
+            [clojure-perf.macros :refer [defbench measure]])
   (:gen-class))
-
-(def ^:private dividing-line-width 80)
-(def ^:private dividing-line-thin (apply str (repeat dividing-line-width \-)))
-(def ^:private dividing-line-thick (apply str (repeat dividing-line-width \=)))
-
-(defmacro measure
-  [& body]
-  (let [expr (if (second body) (cons 'do body) (first body))]
-    `(do (pprint/write '~expr :dispatch pprint/code-dispatch)
-         (pprint/fresh-line)
-         (let [output# (with-out-str (quick-bench ~expr))
-               lines# (-> output# string/trim-newline string/split-lines)]
-           (println ";")
-           (doseq [line# lines#]
-             (println ";" line#)))
-         (println ";" dividing-line-thin)
-         (pprint/fresh-line))))
-
-(defmacro defbench
-  [name args & body]
-  `(defn ~name
-     ~args
-     (println)
-     (println ";" '~name)
-     (println ";" dividing-line-thick)
-     (println)
-     ~@body))
 
 (defbench seq-starts-with
   [a b]
